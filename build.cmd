923542420d3cfa738b92d81bd7a1c8bfef6a31c7
Peter Stromquist <peter@twistedstream.com>
2013-08-05 22:08:14 +0000

    Upgrade to Buildage 2.0 Using version in Git tags instead of Input.targets

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
  @ECHO OFF
- SET BuildCmd=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\build\Buildage\scripts\Build.proj
+ SET BuildCmd=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\build\Buildage\scripts\Build.proj /toolsversion:4.0
  IF [%1]==[] (%BuildCmd%) ELSE (%BuildCmd% /t:%1)
  
