dab41b1a32bb0440830d88dd29a8cdd28eb55ebf
Peter Stromquist <peter@twistedstream.com>
2013-08-07 21:32:35 +0000

    Stub new Internet Web Site project That has Forms Authentication configured

---
+++ src/Sample.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FormsToTokenAccessAuthentication Sample Servoce")]
+[assembly: AssemblyDescription("Sample REST service for FormsToTokenAccessAuthentication.")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1bbd0441-948e-4b59-a041-b853ea35def0")]
