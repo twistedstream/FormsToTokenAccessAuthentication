efb9cdae51bddd3b6f20c7e01b3ef31d5d6379a8
Peter Stromquist <peter@twistedstream.com>
2013-08-08 20:48:27 +0000

    Stub client console app project

---
+++ src/Sample.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TS.FormsToTokenAccessAuthentication.Sample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Sample.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FormsToTokenAccessAuthentication Sample Client")]
+[assembly: AssemblyDescription("Sample client for FormsToTokenAccessAuthentication.")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("580bf17b-e5b9-43e1-87df-506580d1ef3f")]
